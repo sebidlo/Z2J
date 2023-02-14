4ffd1e7ea2343c8ed80242d21d158b8d57a39e98
Sebastian Kycia <s.kycia@advacom.pl>
2023-02-14 16:41:23 +0100

    paragraph broken '\'

--- p-101-l03/main.py
+++ p-101-l03/main.py
@@ -35,3 +35,12 @@ print(string4)
 letters = "abc"
 num_letters = len(letters)
 print(num_letters)
+
+
+paragraph = "This planet has - or rather had - a problem, which was \
+this: most of the people living on it were unhappy for pretty much \
+of the time. Many solutions were suggested for this problem, but \
+most of these were largely concerned with the movements of small \
+green pieces of paper, which is odd because on the whole it wasn't \
+the small green pieces of paper that were unhappy."
+print(paragraph)
