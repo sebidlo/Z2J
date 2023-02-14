88be94700fdecb221bf5087f6828db96be4dad71
Sebastian Kycia <s.kycia@advacom.pl>
2023-02-14 16:09:01 +0100

    print type text_variable

--- p-101-l03/main.py
+++ p-101-l03/main.py
@@ -10,3 +10,7 @@ print("Hello World")
 
 text_variable = "Hellooo !!!"
 print(text_variable)  # This is an in-line comment.
+
+text_type = type(text_variable)
+print("type text_variable is:")
+print(text_type)
