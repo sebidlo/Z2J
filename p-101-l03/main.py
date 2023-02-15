5333fa847371a3152c1e38156f1cc5c53d95ab55
Sebastian Kycia <s.kycia@advacom.pl>
2023-02-15 14:21:06 +0100

    comment old cod

--- p-101-l03/main.py
+++ p-101-l03/main.py
@@ -15,23 +15,24 @@ text_type = type(text_variable)
 print("type text_variable is:")
 print(text_type)
 """
+'''
 string1 = 'Hello, world'
 string2 = "1234"
 print(string1)
 print(string2)
-
+'''
 """
 The quotes surrounding a string are called delimiters because they tell Python where a string begins and where it ends.
 When one type of quotes is used as the delimiter, the other type of quote can be used inside of the string
 """
-
+'''
 string3 = "We're #1!"
 string4 = 'I said, "Put it over by the llama."'
 print(string3)
 print(string4)
-
+'''
 # Determine the Length of a String
-
+'''
 letters = "abc"
 num_letters = len(letters)
 print(num_letters)
@@ -55,3 +56,4 @@ print(paragraph2)
 print("""An example of a
 ...          string that spans across multiple lines
 ...                 that also preserves whitespace.""")
+'''
