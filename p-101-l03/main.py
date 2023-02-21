daa48029e68089acf81027197f53b49384749f97
Sebastian Kycia <s.kycia@advacom.pl>
2023-02-21 15:32:08 +0100

    4.6 Review Exercises - Converting Strings to Numbers - done

--- p-101-l03/main.py
+++ p-101-l03/main.py
@@ -171,34 +171,35 @@ print("Only " + str(total_pancakes - pancakes_eaten) + " pancakes left.")
 1. Create a string containing an integer, then convert that string into
 an actual integer object using int(). Test that your new object is
 a number by multiplying it by another number and displaying the result.
-'''
+
 number = 20
 int_numer = int(number)
 print(int_numer * 2)
 
-'''
+
 2. Repeat the previous exercise, but use a floating-point number and
 float().
-'''
+
 number_2 = 20.0
 float_number = float(number_2)
 print(float_number * 2)
 
-'''
+
 3. Create a string object and an integer object, then display them sideby-
 side with a single print statement by using the str() function.
-'''
+
 string_object = "I drink today"
 string_object_2 = "coffe"
 number_3 = 3
 print(string_object, str(number_3), string_object_2)
 
-'''
+
 4. Write a script that gets two numbers from the user using the
 input() function twice, multiplies the numbers together, and
 displays the result. If the user enters 2 and 4, your program should
 print the following text: The product of 2 and 4 is 8.0.
-'''
+
 num = input("Enter a number: ")
 num_2 = input("Enter a second number:")
 print("The product of number and second number is ", str(int(num) * int(num_2)))
+'''
