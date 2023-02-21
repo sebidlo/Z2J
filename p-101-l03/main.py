f54fb7087bc45375872dd5b32b1896ee99001004
Sebastian Kycia <s.kycia@advacom.pl>
2023-02-21 15:55:42 +0100

    4.7 Review Exercises - formating f-string and .format() - done

--- p-101-l03/main.py
+++ p-101-l03/main.py
@@ -214,13 +214,18 @@ a string object named animal with the value "newt". Then use these
 objects to print the following string using only string concatenation:
 0.2 kg is the weight of the newt.
 '''
+wight = 0.2
+animal = "newt"
+print(str(wight) + " kg is the weight of the " + str(animal) + ".")
 
 '''
 2. Display the same string by using the .format() method and empty
 {} place-holders.
 '''
+print("{} kg is the weight of the {}.".format(wight, animal))
 
 '''
 3. Display the same string using an f-string.
 
 '''
+print(f"{wight} kg is the weight of the {animal}.")
