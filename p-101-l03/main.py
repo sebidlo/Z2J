9c73f674c09d158d574084f013424c8ec796007a
Sebastian Kycia <s.kycia@advacom.pl>
2023-02-21 16:09:54 +0100

    substring, find()

--- p-101-l03/main.py
+++ p-101-l03/main.py
@@ -207,25 +207,26 @@ n = 30
 m = 34
 print(f"{n * m}")
 
-'''
-'''
 1. Create a float object named weight with the value 0.2, and create
 a string object named animal with the value "newt". Then use these
 objects to print the following string using only string concatenation:
 0.2 kg is the weight of the newt.
-'''
+
 wight = 0.2
 animal = "newt"
 print(str(wight) + " kg is the weight of the " + str(animal) + ".")
 
-'''
+
 2. Display the same string by using the .format() method and empty
 {} place-holders.
-'''
+
 print("{} kg is the weight of the {}.".format(wight, animal))
 
-'''
+
 3. Display the same string using an f-string.
 
-'''
+
 print(f"{wight} kg is the weight of the {animal}.")
+'''
+phrase = "the surprise is in here somewhere"
+print(phrase.find("surprise"))
