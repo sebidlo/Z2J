a0474b0ded7f026bcacd3eea7b745e8b4892c78b
Sebastian Kycia <s.kycia@advacom.pl>
2023-02-22 14:42:58 +0100

    5.2 Review Exercises - done

--- p-101-l03/main.py
+++ p-101-l03/main.py
@@ -300,14 +300,22 @@ num1 and num2 should be assigned the integer literal 25,000,000,
 one written with underscored and one without. Print num1 and num2
 on two separate lines.
 '''
-
+num1 = 25000000
+num2 = 25_000_000
+print(num1)
+print(num2)
 '''
 2. Write a script that assigns the floating-point literal 175000.0 to the
 variable num using exponential notation, and then prints num in the
 interactive window.
 '''
+num = 175000.0
+print(num)
 
 '''
 3. In IDLE’s interactive window, try and find the smallest exponent N
 so that 2e<N>, where <N> is replaced with your number, returns inf.
 '''
+print(2e300)
+print(2e307)
+print(2e308)
