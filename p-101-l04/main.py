d34c0dd55db2e15666dc0854f4fc18dd3a57c2e6
Sebastian Kycia <sebidlo@gmail.com>
2023-03-09 21:41:39 +0100

    Write Your Own Functions - add signature

--- p-101-l04/main.py
+++ p-101-l04/main.py
@@ -4,10 +4,11 @@ num_letters = len("four")
 print(num_letters)
 return_value = print("What do I return?")
 print(return_value)
-
+'''
 
 def multiply(x, y):
     # Function signature
+    """Return the product of two numbers x and y."""
     # # Function body
     product = x * y
     return product
@@ -18,8 +19,10 @@ print(multiply(2,2))
 num = multiply(2,5)
 
 print(num)
-'''
+
 def greet(name):
     print(f"Hello {name}")
 
 greet("Sebastian")
+
+help(multiply)
