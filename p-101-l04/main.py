37dffe8f9184c54f425d96d5d6258b6ea73b5cad
Sebastian Kycia <sebidlo@gmail.com>
2023-03-10 19:31:09 +0100

    Loop for, renge

--- p-101-l04/main.py
+++ p-101-l04/main.py
@@ -59,7 +59,7 @@ n = 1
 while n < 5:
     print(n)
     n = n + 1
-'''
+
 for letter in "Python":
     print(letter)
 
@@ -69,3 +69,13 @@ index = 0
 while index < len(word):
     print(word[index])
     index = index + 1
+'''
+for n in range(3):
+    print("Python")
+
+for n in range(10, 20):
+    print(n * n)
+
+amount = float(input("Enter an amount: "))
+for num_people in range(2, 6):
+    print(f"{num_people} people: ${amount / num_people:,.2f} each")
