6ae3083108891689372f7762bff857aac57ff2aa
Sebastian Kycia <sebidlo@gmail.com>
2023-03-14 21:45:13 +0100

    error 1

--- p-101-l04/main.py
+++ p-101-l04/main.py
@@ -187,7 +187,16 @@ def add_to_total(n):
 add_to_total(5)
 print(total)
 
-'''
-for i in range(1, 4):
+
     j = i * 2
     print(f"i is {i} and j is {j}")
+'''
+
+def add_underscores(word):
+    new_word = "_"
+    for i in range(0, len(word)):
+        new_word = word[i] + "_"
+        return new_word
+
+phrase = "hello"
+print(add_underscores(phrase))
