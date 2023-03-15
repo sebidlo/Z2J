a8a6620d20ef9a625ce1879ead44732fcd9f0267
Sebastian Kycia <sebidlo@gmail.com>
2023-03-15 20:29:25 +0100

    if in if example

--- p-101-l05/main.py
+++ p-101-l05/main.py
@@ -23,7 +23,7 @@ else:
     print("You did not pass the class :(")
 
 print("Thank you for attending.")
-'''
+
 grade = 85 # 1
 
 if grade >= 90: # 2
@@ -35,3 +35,26 @@ elif grade >= 70: # 4
 else: # 5
     print("You did not pass the class :(")
 print("Thanks for attending.") # 6
+'''
+sport = input("Enter a sport: ")    
+p1_score = int(input("Enter player 1 score: "))
+p2_score = int(input("Enter player 2 score: "))
+# 1
+if sport.lower() == "basketball":
+    if p1_score == p2_score:
+        print("The game is a draw.")
+    elif p1_score > p2_score:
+        print("Player 1 wins.")
+    else:
+        print("Player 2 wins.")
+# 2
+elif sport.lower() == "golf":
+    if p1_score == p2_score:
+        print("The game is a draw.")
+    elif p1_score < p2_score:
+        print("Player 1 wins.")
+    else:
+        print("Player 2 wins.")
+# 3
+else:
+    print("Unknown sport")
