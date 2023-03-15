7bb5f81fbd1a742680ea7c969744e4ad4b762700
Sebastian Kycia <sebidlo@gmail.com>
2023-03-15 20:09:06 +0100

    IF parts

--- p-101-l05/main.py
+++ p-101-l05/main.py
@@ -1,1 +1,9 @@
 # Conditional Logic and Control Flow
+
+'''
+1. The if keyword
+2. A test condition, followed by a colon
+3. An indented block of code that is executed if the test condition is True
+'''    
+if 2 + 2 == 4:
+    print("2 and 2 is 4")
