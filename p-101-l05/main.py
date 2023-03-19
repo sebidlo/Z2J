1fbb0e4c5b6d0a42ea677b5d29ba3cffa59ec273
Sebastian Kycia <sebidlo@gmail.com>
2023-03-19 12:29:39 +0100

    if in loop for

--- p-101-l05/main.py
+++ p-101-l05/main.py
@@ -101,3 +101,9 @@ else:
     
 8.4. Challenge: Find the Factors of a Number    
 '''
+
+sum_of_evens = 0
+for n in range(1, 100):
+    if n % 2 == 0:
+        sum_of_evens = sum_of_evens + n
+print(sum_of_evens)
