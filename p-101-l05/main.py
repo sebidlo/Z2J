e3c8b69192e76bc0b857b94cb2afd30784d22f5d
Sebastian Kycia <sebidlo@gmail.com>
2023-03-19 13:02:47 +0100

    8.6 Review - done

--- p-101-l05/main.py
+++ p-101-l05/main.py
@@ -148,7 +148,17 @@ Review Exercises 8.6
 1. Using break , write a program that repeatedly asks the user for some
 input and only quits if the user enters "q" or "Q" .
 '''
+for n in range(100):
+    znak = input("Wpisz jakis znak z klawiatury: ")
+    if znak == 'q':
+        break
+
 '''
 2. Using continue , write a program that loops over the numbers 1 to
 50 and prints all numbers that are not multiples of 3 .
 '''
+
+for i in range(1, 50):
+    if i % 3 == 0:
+        continue
+    print(i)
