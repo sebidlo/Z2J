4b56e4f1da4edc8482fc97126970e2e714501f26
Sebastian Kycia <sebidlo@gmail.com>
2023-03-19 12:35:13 +0100

    continue in loop for

--- p-101-l05/main.py
+++ p-101-l05/main.py
@@ -113,5 +113,12 @@ for n in range(0, 4):
     if n == 2:
         break
     print(n)
-    
+
 print(f"Finished with n = {n}")
+
+for i in range(0, 4):
+    if i == 2:
+        continue
+    print(i)
+    
+print(f"Finished with i = {i}")
