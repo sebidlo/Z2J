067bed95d82adb38952549470fd3307dd8e1dc14
Sebastian Kycia <s.kycia@advacom.pl>
2023-03-20 16:19:13 +0100

    8.7 Review

--- p-101-l05/main.py
+++ p-101-l05/main.py
@@ -239,7 +239,7 @@ try:
 except ValueError:
     print("That was not an integer")
 
-'''
+
 
 
 def divide(num1, num2):
@@ -249,3 +249,23 @@ def divide(num1, num2):
         print("Both arguments must be numbers")
     except ZeroDivisionError:
         print("num2 must not be 0")
+
+8.7 Review Exercises
+
+1. Write a script that repeatedly asks the user to input an integer,
+displaying a message to “try again” by catching the ValueError that
+is raised if the user did not enter an integer.
+Once the user enters an integer, the program should display
+the number back to the user and end without crashing.
+'''
+'''
+2. Write a program that asks the user to input a string and an integer
+n. Then display the character at index n in the string.
+Use error handling to make sure the program doesn’t crash
+if the user does not enter an integer or the index is out of bounds.
+The program should display a different message depending on
+what error occurs.
+'''
+
+
+
