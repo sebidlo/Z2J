0d78f081c629636416ff2474370df7668d5cf60d
Sebastian Kycia <s.kycia@advacom.pl>
2023-03-24 14:35:49 +0100

    random.randint - Flipping Fair Coins

--- p-101-l05/main.py
+++ p-101-l05/main.py
@@ -295,6 +295,31 @@ while ok is False:
 
         randpom module
 
-'''
-import random
+
+# import random
 print(random.randint(1, 10))
+'''
+
+
+
+
+import random
+def coin_flip():
+    """Randomly return 'heads' or 'tails'."""
+    if random.randint(0, 1) == 0:
+        return "heads"
+    else:
+        return "tails"
+
+
+# First initialize the tallies to 0
+heads_tally = 0
+tails_tally = 0
+for trial in range(10_000):
+    if coin_flip() == "heads":
+        heads_tally = heads_tally + 1
+    else:
+        tails_tally = tails_tally + 1
+
+ratio = heads_tally / tails_tally
+print(f"The ratio of heads to tails is {ratio}")
