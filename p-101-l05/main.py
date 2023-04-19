163e144feffd2d1ec570d6152bdb59e43ec89069
Sebastian Kycia <s.kycia@advacom.pl>
2023-04-19 11:11:32 +0200

    modification gitignore and add text Review 9.2

--- p-101-l05/main.py
+++ p-101-l05/main.py
@@ -636,3 +636,33 @@ str_numbers = ["1.5", "2.3", "5.25"]
 float_numbers = [float(value) for value in str_numbers]
 print(float_numbers)
 '''
+'''
+1. Create a tuple literal named cardinal_numbers that holds the strings
+"first", "second" and "third", in that order.
+'''
+
+'''
+2. Using index notation and print(), display the string at index 1 in
+cardinal_numbers.
+'''
+
+'''
+3. Unpack the values in cardinal_numbers into three new strings
+named position1, position2 and position3 in a single line of code,
+then print each value on a separate line.
+'''
+
+'''
+4. Create a tuple called my_name that contains the letters of your name
+by using tuple() and a string literal.
+'''
+
+'''
+4. Create a tuple called my_name that contains the letters of your name
+by using tuple() and a string literal.
+'''
+
+'''
+6. Create a new tuple containing all but the first letter in my_name using
+slicing notation.
+'''
