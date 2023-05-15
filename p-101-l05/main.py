355ffb09ab3aeab3b39c86e2f579148d4ace6537
Sebastian Kycia <sebidlo@gmail.com>
2023-05-15 20:32:02 +0200

    Review Exercises 9.2 done

--- p-101-l05/main.py
+++ p-101-l05/main.py
@@ -640,29 +640,38 @@ print(float_numbers)
 1. Create a tuple literal named cardinal_numbers that holds the strings
 "first", "second" and "third", in that order.
 '''
+cardinal_numbers = "first", "second", "third" 
+print(cardinal_numbers)
+print(type(cardinal_numbers))
 
 '''
 2. Using index notation and print(), display the string at index 1 in
 cardinal_numbers.
 '''
+print(cardinal_numbers[1])
 
 '''
 3. Unpack the values in cardinal_numbers into three new strings
 named position1, position2 and position3 in a single line of code,
 then print each value on a separate line.
 '''
+position1, position2, position3 = cardinal_numbers
+print(position1)
+print(position2)
+print(position3)
+
 
 '''
 4. Create a tuple called my_name that contains the letters of your name
 by using tuple() and a string literal.
 '''
-
-'''
-4. Create a tuple called my_name that contains the letters of your name
-by using tuple() and a string literal.
+my_name = tuple("sebastian")
+print(my_name)
 '''
 
-'''
 6. Create a new tuple containing all but the first letter in my_name using
 slicing notation.
 '''
+
+my_name_2 = my_name[1:]
+print(my_name_2)
